674a973ee9c37d6cd9ab85643df358305d2073a5
Quincunx33 <2.07334025e+08+Quincunx33@users.noreply.github.com>
2026-08-28 04:11:53 +0000

    Add ACPI-aware IOAPIC routing controls

--- arch/x86/isr.c
+++ arch/x86/isr.c
@@ -6,6 +6,7 @@
 #include "syscall.h"
 #include "scheduler.h"
 #include "apic.h"
+#include "ioapic.h"
 
 typedef struct idt_entry { uint16_t base_low; uint16_t selector; uint8_t zero; uint8_t flags; uint16_t base_high; } __attribute__((packed)) idt_entry_t;
 typedef struct idt_ptr { uint16_t limit; uint32_t base; } __attribute__((packed)) idt_ptr_t;
--- include/ioapic.h
+++ include/ioapic.h
@@ -4,5 +4,5 @@
 void ioapic_init(void);
 uint32_t ioapic_read(uint8_t reg);
 void ioapic_write(uint8_t reg,uint32_t value);
-int ioapic_available(void);void ioapic_route_irq(uint8_t irq,uint8_t vector,uint8_t destination,uint8_t masked);
+int ioapic_available(void);void ioapic_route_irq(uint8_t irq,uint8_t vector,uint8_t destination,uint8_t masked);void ioapic_enable_irq(uint8_t irq);void ioapic_disable_irq(uint8_t irq);
 #endif
